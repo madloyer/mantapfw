9f307127aa7d50b8ca4ea9f7639cc644cddad1c4
madloyer <87596107+madloyer@users.noreply.github.com>
2021-08-06 19:37:48 +0700

    Update forward.py

--- Plugins/forward.py
+++ Plugins/forward.py
@@ -7,6 +7,8 @@ import asyncio
 from pyrogram import filters
 from bot import channelforward
 from config import Config 
+from bot import autocaption
+from database.database import *
 
 @channelforward.on_message(filters.channel)
 async def forward(c, m):
@@ -18,3 +20,48 @@ async def forward(c, m):
           await m.forward(int(to_channel), as_copy=True)
           print("Forwarded a message from", from_channel, "to", to_channel)
           asyncio.sleep(1)
+# =
+usercaption_position = Config.CAPTION_POSITION
+caption_position = usercaption_position.lower()
+
+
+@autocaption.on_message(filters.channel & (filters.document | filters.video | filters.audio ) & ~filters.edited, group=-1)
+async def editing(bot, message):
+      caption_text = await get_caption(Config.ADMIN_ID)
+      if caption_text == None:
+          caption_text = Config.CAPTION_TEXT
+          if not caption_text:
+              return
+      caption_text = caption_text.caption
+      try:
+          if (message.document or message.video or message.audio):
+             file_caption = f"**{message.caption}**"
+      except:
+          pass
+      try:
+          if caption_position == "top":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = caption_text + "\n" + file_caption,
+                 parse_mode = "markdown"
+             )
+          elif caption_position == "bottom":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id, 
+                 message_id = message.message_id,
+                 caption = file_caption + "\n" + caption_text,
+                 parse_mode = "markdown"
+             )
+          elif caption_position == "nil":
+             await bot.edit_message_caption(
+                 chat_id = message.chat.id,
+                 message_id = message.message_id,
+                 caption = caption_text, 
+                 parse_mode = "markdown"
+             ) 
+      except:
+          pass
+              
+                   
+      
