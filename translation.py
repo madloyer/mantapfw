a44f9106f725ab2ac9ab8802defa72020a7e2d82
madloyer <87596107+madloyer@users.noreply.github.com>
2021-08-06 19:54:38 +0700

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,9 @@
+class Translation(object):
+
+      # Start text
+      START = "Hi I am a channel auto forward bot clone Mantapjozz"             
+
+      #About text
+      ABOUT = """
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
